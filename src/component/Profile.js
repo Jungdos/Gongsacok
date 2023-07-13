229bb60a0fb9eeca1343886209d9e3c61db17b68
Junwon <was607412@naver.com>
2023-07-13 11:16:44 +0900

    Add to ListCompany with comment

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,80 +1,137 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'; 
+import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
-import Avatar from '@material-ui/core/Avatar';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
+// CSS 스타일을 설정
+const useStyles = makeStyles({
+  table: { 
+    minWidth: 500 //테이블의 최소 넓이 설정
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
+  pagination: { 
+    display: "flex",  //페이지네이션 섹션을 flex로 설정
+    justifyContent: "center", //섹션 내부 요소들을 중앙에 배치
+    marginTop: 10 //페이지네이션 섹션의 상단 마진 설정
   },
-  title: {
-    flexGrow: 1,
-  },
-  large: {
-    width: theme.spacing(20),
-    height: theme.spacing(20),
-  },
-}));
+  pageNumber: { 
+    cursor: "pointer", // 페이지 번호위에 마우를 가져다놓으면 포인터로 바뀜
+    marginRight: 10  //페이지 번호의 오른쪽 마진셜정
+  }
+});
 
-export default function Profile() {
-  const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const user = JSON.parse(localStorage.getItem('user'));
+const Profile = () => {
+  const classes = useStyles(); // 스타일 설정을 사용함.
+  // 상태 설정 함수들을 선언함.
+  const [inputData, setInputData] = useState([]);
+  const [totalElements, setTotalElements] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(0);
 
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+  // 위치, 반경, 공사 유형과 같은 몇 가지 변수를 선언함.
+  const longitude = 12692971;
+  const latitude = 3752519;
+  const radius = 10000;
+  const gongsaType = "emer";
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  // 컴포넌트가 렌더링될 때 실행되는 useEffect 훅입니다.
+  useEffect(() => {
+    // 데이터를 가져오는 비동기 함수를 선언함.
+    const fetchData = async () => {
+      try {
+        setLoading(true); // 로딩 상태를 true로 설정
+        setError(null);// 에러 상태를 null로 설정
 
-  const handleLogout = () => {
-    localStorage.removeItem("accessToken");
-    localStorage.removeItem("user");
-    window.location.href = "/";
-  };
+        // API로부터 데이터를 가져옵니다.
 
+        const res = await axios.post(
+          'https://devawsback.gongsacok.com/pub/listCompany',
+          { offset: page * 2, //현재 페이지에 따라 offset을 설정
+            size: 2, // 페이지 당 사이즈는 2로 설정
+            longitude, 
+            latitude, 
+            radius, 
+            gongsaType }
+        );
+
+        // 응답에서 필요한 데이터만 추출함.
+        const _inputData = res.data.data.map(rowData => ({
+          cid: rowData.cid,
+          name: rowData.name,
+          createTime: rowData.createTime,
+          updateTime: rowData.updateTime
+        }));
+
+        // 상태를 업데이트함.
+        setInputData(_inputData);  // 데이터를 저장
+        setTotalElements(res.data.page.totalElements); // 총 요소 수를 저장
+        setTotalPages(res.data.page.totalPages); // 총 페이지 수를 저장
+        setLoading(false); // 로딩 상태를 false로 설정
+      } catch (e) {
+        // 에러가 발생하면 상태를 업데이트함.
+        setError('데이터를 가져오는 데 실패했습니다'); // 에러 상태를 설정
+        setLoading(false); // 로딩 상태를 false로 설정
+      }
+    };
+
+    // 데이터를 가져옵니다.
+    fetchData();
+  }, [page]); //페이지가 바뀔 떄마다 useEffect를 재실행
+    //반환하는 JSX에서 페이지 번호를 클릭하면 setPage를 호출하여 현재 페이지를 변경함
+
+  // 로딩 중이거나 에러가 발생했을 때 적절한 컴포넌트를 렌더링함.
+  if (loading) return <CircularProgress />;
+  if (error) return <Typography variant="h6">오류: {error}</Typography>;
+
+  // 데이터를 성공적으로 로드했을 때 테이블을 렌더링함.
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            프로필
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="간단한 테이블">
+        <TableHead>
+          <TableRow>
+            <TableCell>관리 번호</TableCell>
+            <TableCell>회사명</TableCell>
+            <TableCell>생성 시간</TableCell>
+            <TableCell>업데이트 시간</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {inputData.map((row, index) => (
+            <TableRow key={index}>
+              <TableCell>{row.cid}</TableCell>
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.createTime}</TableCell>
+              <TableCell>{row.updateTime}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <Typography variant="subtitle1">총 운영자 수: {totalElements}</Typography>
+      <Typography variant="subtitle1">총 페이지 수: {totalPages}</Typography>
+
+      <div className={classes.pagination}>
+        {[...Array(totalPages).keys()].map(num => (
+          <Typography
+            key={num}
+            className={classes.pageNumber}
+            onClick={() => setPage(num)}
+            color={num === page ? "secondary" : "primary"}
+          >
+            {num + 1}
           </Typography>
-            <div>
-            <IconButton onClick={handleMenu} color="inherit">
-              <Avatar src={user.avatar} />
-            </IconButton>
-            <Menu id="menu-appbar" 
-              anchorEl={anchorEl} 
-              open={open}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleLogout}>로그아웃</MenuItem>
-            </Menu>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Card className={classes.root} variant="outlined">
-        <CardContent>
-          <Avatar src={user.avatar} className={classes.large} />
-          <Typography variant="h5">
-          환영합니다! {user.fname} {user.lname}
-          </Typography>
-        </CardContent>
-      </Card>
-    </div>
+        ))}
+      </div>
+    </TableContainer>
   );
-}
+};
+
+export default Profile; // Profile 컴포넌트를 내보냅니다.
--- src/component/Sigin.js
+++ src/component/Sigin.js
@@ -9,7 +9,9 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import swal from 'sweetalert';
+//표시창 인터페이스 라이브러리를 가져오기.
 import axios from 'axios';
+//axios 라이브러리 가져오기.
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -40,22 +42,23 @@ const useStyles = makeStyles((theme) => ({
 
 async function loginUser(credentials) {
   return axios({
+        //axios에 사용될수있는 매서드. 
     method: 'post',
     url: 'https://devawsback.gongsacok.com/pub/login',
     headers: {
       'Content-Type': 'application/json'
     },
     data: credentials
-    //axios에 사용될수있는 매서드. 
-    
+    //credentials => loginIser비동기함수에 전달되는 매개변수
+
   })
-  .then(response => response.data)
-  //리턴값을 제대로 받았다면 response.data를 반환한다.
-  .catch(error => {
-    console.error('There was an error!', error);
-    throw error;
-  });
-  //리턴값을 제대로 받지 못했다면 브라우저 콘솔창에 error메레지를 표시한다.
+    .then(response => response.data)
+    //리턴값을 제대로 받았다면 response.data를 반환한다.
+    .catch(error => {
+      console.error('There was an error!', error);
+      throw error;
+    });
+  //리턴값을 제대로 받지 못했다면 브라우저 콘솔창에 error메세지를 표시한다.
 }
 
 async function registerUser(userData) {
@@ -68,38 +71,38 @@ async function registerUser(userData) {
     },
     data: userData
   })
-  .then(response => {
-    //데이터를 받아오면서
-    if (response.data.status === 'success') {
-      //만약 리턴값의 status가 'success'라면
-      swal('Success', 'You have successfully registered!', 'success', {
-        //swal 표시 인터페이스를 통해 'success' , "You have successfully registered"라는 창을 띄운다
-        buttons: false,
-        timer: 2000,
-      }).then(() => {
-        window.location.href = '/Profile';
-        //그리고 리턴값을 제대로 받았다면 /Profile로 페이지 전환을 시킨다.
-      });
-    } else {
-      swal('Failed', 'Registration failed. Please check your inputs.', 'error');
-    }   //리턴값이 제대로 받아지지않았을 경우엔
-        //swal표시 인터페이스를 통해 'Failed' , "Registration failed. Please check your inputs"
-        // 라는 창을 띄우고 데이터를 반환한다.
-    return response.data;
-  })
-  .catch(error => {
-    console.error('There was an error!', error);
-    swal('Error', 'An error occurred during registration. Please try again later.', 'error');
-    throw error;
-        //axios 요청중에 오류가 발생한다면 다음과같은 메세지가 콘솔창에 뜨게한다
-        //There was an error!
-        //그리고 사용자 인터페이스에서도 swal을 통해 
-        //Error', "An error occurred during registration. Please try again later." 메세지를 표시한다
-  });
+    .then(response => {
+      //데이터를 받아오면서
+      if (response.data.status === 'success') {
+        //만약 리턴값의 status가 'success'라면
+        swal('Success', 'You have successfully registered!', 'success', {
+          //swal 라이브러리 인터페이스를 통해 'success' , "You have successfully registered"라는 창을 띄운다
+          buttons: false,
+          timer: 2000,
+        }).then(() => {
+          window.location.href = '/Profile';
+          //그리고 리턴값을 제대로 받았다면 /Profile로 페이지 전환을 시킨다.
+        });
+      } else {
+        swal('Failed', 'Registration failed. Please check your inputs.', 'error');
+      }   //리턴값이 제대로 받아지지않았을 경우엔
+      //swal표시 인터페이스를 통해 'Failed' , "Registration failed. Please check your inputs"
+      // 라는 창을 띄우고 데이터를 반환한다.
+      return response.data;
+    })
+    .catch(error => {
+      console.error('There was an error!', error);
+      swal('Error', 'An error occurred during registration. Please try again later.', 'error');
+      throw error;
+      //axios 요청중에 오류가 발생한다면 다음과같은 메세지가 콘솔창에 뜨게한다
+      //There was an error!
+      //그리고 사용자 인터페이스에서도 swal을 통해 
+      //Error', "An error occurred during registration. Please try again later." 메세지를 표시한다
+    });
 }
-        //.then 과 .catch의 부정적인 메세지표현은 비슷하나 둘의 차이점은
-        //.then은 서버로부터 응답에 대한 메세지를 표시한것이고
-        //.catch는 Axios를 통한 요청에 대한 응답의 메세지를 표시한것이다.
+//.then 과 .catch의 부정적인 메세지표현은 비슷하나 둘의 차이점은
+//.then은 서버로부터 응답에 대한 메세지를 표시한것이고
+//.catch는 Axios를 통한 요청에 대한 응답의 메세지를 표시한것이다.
 
 export default function Signin() {
   const classes = useStyles();
@@ -108,47 +111,47 @@ export default function Signin() {
   const [password, setPassword] = useState('');
   const [usernameRegister, setUsernameRegister] = useState('');
   const [passwordRegister, setPasswordRegister] = useState('');
-        //아이디와 비밀번호, 등록할아이디와 등록할 비밀번호를 useState를 통해 변수로 저장한다.
-        //유동적으로 수정할수있도록 set 을 붙여서 하나씩 더만들어준다.
+  //아이디와 비밀번호, 등록할아이디와 등록할 비밀번호를 useState를 통해 변수로 저장한다.
+  //유동적으로 수정할수있도록 set 을 붙여서 하나씩 더만들어준다.
   const handleSubmit = async (e) => {
     e.preventDefault();
-        //asund (e) 라는 handleSubmit 함수를 실행하면서 페이지가 새로고침되는걸 막아준다.
+    //asund (e) 라는 handleSubmit 함수를 실행하면서 페이지가 새로고침되는걸 막아준다.
     try {
       const response = await loginUser({
-        //try 라는 박스 안에서 axios요청으로 인해받아오는걸 시도하고 대기한다.
+        //try 박스 안에서 axios요청으로 인해받아오는걸 시도하고 대기한다.
         //loginUser의 함수를
         userid: username,
         passwd: password
       });
-        // 그내용은 userid , passwd
+      // 그내용은 userid , passwd
       console.log('Login API response:', response);
-        // 콘솔창에 Login API response 를 서버에서 받아온다.
+      // 콘솔창에 Login API response 를 서버에서 받아온다.
       if (response.status === 'success') {
         //만약 받아온 status가 'success'일경우
-        swal('Success', 'You have successfully logged in!', 'success', {
-        //swal 인터페이스 라이브러리로 인한 표시창을 띄운다.
-        // 그내용은 '성공', '로그인에 성공하였습니다.'
+        swal('성공', '로그인에 성공하였습니당', 'success', {
+          //swal 인터페이스 라이브러리로 인한 표시창을 띄운다.
+          // 그내용은 '성공', '로그인에 성공하였습니다.'
           buttons: false,
           timer: 2000,
         }).then(() => {
-        //요청한 값을 받아온다면
+          //요청한 값을 받아온다면
           localStorage.setItem('jtoken', response.data.jtoken);
           localStorage.setItem('user', JSON.stringify(response.data));
           window.location.href = '/profile';
-        //서버에서 jtoken 과 user값을 받아와서 로컬스토리지에 저장한다.
-        //저장한 후엔 /profile 페이지로 페이지 전환이 일어난다.
+          //서버에서 jtoken 과 user값을 받아와서 로컬스토리지에 저장한다.
+          //저장한 후엔 /profile 페이지로 페이지 전환이 일어난다.
         });
       } else {
         //서버에 요청한 값을 받아오지못한다면.
         swal('Failed', 'Login failed. Please check your username and password.', 'error');
       }
-        //swal인터페이스 라이브러리로 인한 표시창을 띄운다.
-        //그 내용은 '실패', '로그인실패. 아이디와 비밀번호를 확인해주세요!.' 
+      //swal인터페이스 라이브러리로 인한 표시창을 띄운다.
+      //그 내용은 '실패', '로그인실패. 아이디와 비밀번호를 확인해주세요!.' 
     } catch (error) {
       swal('Error', 'An error occurred during login. Please try again later.', 'error');
     }
-        // Axios요청에 의한 응답이 없을경우 다음과 같은 메세지를 swal라이브러리로 창을 띄운다.
-        //'에러', '로그인이 지연되고있습니다. 다시 시도해주세요.'
+    // Axios요청에 의한 응답이 없을경우 다음과 같은 메세지를 swal라이브러리로 창을 띄운다.
+    //'에러', '로그인이 지연되고있습니다. 다시 시도해주세요.'
   };
 
   const handleSubmitRegister = async (e) => {
@@ -158,7 +161,7 @@ export default function Signin() {
         userid: usernameRegister,
         passwd: passwordRegister
       });
-  
+
       if (response.status === 'success') {
         swal('Success', 'You have successfully registered!', 'success', {
           buttons: false,
@@ -219,6 +222,11 @@ export default function Signin() {
               로그인
             </Button>
           </form>
+          <div className='search_user_info_div'>
+            <div> <b style={{ 'marginLeft' : '15px'}}>아이디찾기 </b></div>
+            <div> <b> 비밀번호 찾기</b> </div>
+          </div>
+
           <Typography component="h1" variant="h5">
 
           </Typography>
